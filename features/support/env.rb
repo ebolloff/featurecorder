7c8ed368cb0bf66c98cc7d811fa095a9bbf07460
Eileen Bolloff <ebolloff@tzi.de>
2015-08-18 19:51:22 +0200

    Add aruba integration for simplecov

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,14 @@
 require 'aruba/cucumber'
 require 'coveralls'
+require 'simplecov'
+
+SimpleCov.command_name 'Cucumber'
+
+Before do
+  # This is using the aruba helper,
+  # cf. https://github.com/cucumber/aruba/blob/master/lib/aruba/api.rb
+  set_environment_variable('COVERAGE', 'true')
+  # This could also be accomplished with the "I set the environment variables to:" step
+end
 
 Coveralls.wear!
